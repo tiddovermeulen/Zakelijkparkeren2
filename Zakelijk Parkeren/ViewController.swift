07466f49481cfd1dd11a7a60e1d11ecbb29dd0c9
Tiddo Vermeulen <tiddovermeulen@tiddos-imac.fritz.box>
2017-01-14 18:27:01 +0100

    Initial Commit

---
+++ Zakelijk Parkeren/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zakelijk Parkeren
+//
+//  Created by Tiddo Vermeulen on 14/01/2017.
+//  Copyright © 2017 Tiddo Vermeulen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
